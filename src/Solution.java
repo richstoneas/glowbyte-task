32e3a961fc806fb804c0b38195b9ce71b0de4589
Байтасов Аян Сакенович <asbajjtasov@sberinsur.ru>
2020-08-20 17:19:53 +0300

    fix sum case

--- src/Solution.java
+++ src/Solution.java
@@ -20,17 +20,15 @@ public class Solution {
 
         System.out.println("Enter the K integer: ");
         int k = scanner.nextInt();
-        System.out.println(calculate(arr, k));
+        System.out.println(calculateBruteForce(arr, k));
     }
 
-    public boolean calculate(ArrayList<Integer> arr, int k) {
-
+    public boolean calculateBruteForce(ArrayList<Integer> arr, int k) {
         int counter = 0;
-
         for(int i = 0; i < arr.size(); i++) {
             for(int j = i + 1; j < arr.size(); j++) {
-                if(((arr.get(i) + arr.get(j)) % k == 0)) {
-                    System.out.println("(" + arr.get(i) + "," + arr.get(j) + ")");
+                int sum = arr.get(i) + arr.get(j);
+                if(sum % k == 0 && sum != 0) {
                     counter++;
                     if(counter == arr.size()/2) {
                         return true;
